6f4de1f1f881d5ef2b33ef58b9a7f72581d489af
trisua <tri@swmff.org>
2024-12-13 20:41:55 -0500

    add: docs config

--- crates/beambin-core/src/lib.rs
+++ crates/beambin-core/src/lib.rs
@@ -1,3 +1,6 @@
+//! 🐝 Beambin!
+#![doc = include_str!("../../../README.md")]
+#![doc(issue_tracker_base_url = "https://github.com/trisuaso/beambin/issues")]
 pub mod api;
 pub mod config;
 pub mod database;
